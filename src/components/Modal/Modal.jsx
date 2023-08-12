23f2b16428d2a3024bcf8e488b1cbb6dfcc1c3d9
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-12 15:24:30 +0300

    Add component Modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import css from './Modal.module.css';
+
+export class Modal extends Component {
+  state = {
+    hidden: true,
+  };
+
+  render() {
+    <div className={css.overlay}>
+      <div className={css.modal}>
+        <img src="" alt="" />
+      </div>
+    </div>;
+  }
+}
