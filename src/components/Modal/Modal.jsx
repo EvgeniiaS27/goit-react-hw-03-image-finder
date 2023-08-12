038f12ac96082fdb7b7d7bc935e2f69df5973bd3
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-12 20:49:16 +0300

    Update component Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,36 @@
+import PropTypes from 'prop-types';
 import { Component } from 'react';
 import css from './Modal.module.css';
 
 export class Modal extends Component {
-  state = {
-    hidden: true,
+  static propTypes = {
+    onCloseModal: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress = e => {
+    if (e.key === 'Escape') {
+      this.props.onCloseModal();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onCloseModal();
+    }
   };
   render() {
     return (
-      <div className={css.overlay}>
+      <div className={css.overlay} onClick={this.handleBackdropClick}>
         <div className={css.modal}>
-          <img src="" alt="" />
+          <img src={this.props.image} alt="" />
         </div>
       </div>
     );
