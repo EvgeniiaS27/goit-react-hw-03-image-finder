7a22e89352612337b3c1397f5770fc2ccc74110c
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-12 15:32:13 +0300

    Update Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,15 +2,14 @@ import { Component } from 'react';
 import css from './Modal.module.css';
 
 export class Modal extends Component {
-  state = {
-    hidden: true,
-  };
-
-  render() {
-    <div className={css.overlay}>
-      <div className={css.modal}>
-        <img src="" alt="" />
-      </div>
-    </div>;
-  }
+  // state = {
+  //   hidden: true,
+  // };
+  // render() {
+  //   <div className={css.overlay}>
+  //     <div className={css.modal}>
+  //       <img src="" alt="" />
+  //     </div>
+  //   </div>;
+  // }
 }
