8ea8d1bc68bed7d47d0e19bbfce0a1b28ffcf20e
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-12 20:48:18 +0300

    Update component ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,16 +1,24 @@
+import PropTypes from 'prop-types';
 import { Component } from 'react';
 import css from './ImageGallery.module.css';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 import { Loader } from 'components/Loader/Loader';
 import { Button } from 'components/Button/Button';
+import { Modal } from 'components/Modal/Modal';
 import { getImages } from '../../services/getImages';
 
 export class ImageGallery extends Component {
+  static propTypes = {
+    query: PropTypes.string.isRequired,
+  };
+
   state = {
     images: [],
     loading: false,
     error: null,
     page: 1,
+    isOpenModal: false,
+    activeImage: '',
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -29,39 +37,86 @@ export class ImageGallery extends Component {
           } else return res.json();
         })
         .then(images => {
-          console.log(images);
           if (images.hits.length !== 0) {
             return this.setState(prevState => ({
-              images: [...prevState.images, images.hits],
+              images: [...prevState.images, ...images.hits],
+            }));
+          } else
+            return Promise.reject(
+              new Error(
+                `OSorry, there are no images matching your search query. Please try again`
+              )
+            );
+        })
+
+        .catch(error => this.setState({ error }))
+        .finally(() => this.setState({ loading: false }));
+    }
+
+    if (prevState.page !== page) {
+      this.setState({ loading: true, error: null });
+
+      getImages(nextQuery, page)
+        .then(res => {
+          if (res.status !== 200) {
+            return Promise.reject(
+              new Error(
+                `Sorry, there are no images matching your search query. Please try again`
+              )
+            );
+          } else return res.json();
+        })
+        .then(images => {
+          if (images.hits.length !== 0) {
+            return this.setState(prevState => ({
+              images: [...prevState.images, ...images.hits],
             }));
           } else
             return Promise.reject(new Error(`Oops, something went wrong...`));
         })
-
-        // getImages(nextQuery, page)
-        //   .then(images => this.setState({ images: images.hits }))
         .catch(error => this.setState({ error }))
         .finally(() => this.setState({ loading: false }));
     }
   }
 
   handleButtonClick = () => {
-    const { page } = this.state.page;
-
     this.setState(prevState => ({
-      page: prevState[page] + 1,
+      page: prevState.page + 1,
     }));
   };
 
+  handleImageClick = activeImage => {
+    this.setState({ activeImage, isOpenModal: true });
+  };
+
+  handelImageMogalClose = () => {
+    this.setState({ activeImage: '', isOpenModal: false });
+  };
+
   render() {
-    const { error, loading, images } = this.state;
+    const { error, loading, images, isOpenModal, activeImage } = this.state;
     return (
       <>
         {error && <h1>{error.massege}</h1>}
         {loading && <Loader />}
+        {isOpenModal && (
+          <Modal
+            image={activeImage}
+            onCloseModal={this.handelImageMogalClose}
+          />
+        )}
+
         <ul className={css.imageGallery}>
           {images.map(({ id, webformatURL, largeImageURL, tags }) => {
-            return <ImageGalleryItem key={id} img={webformatURL} alt={tags} />;
+            return (
+              <ImageGalleryItem
+                key={id}
+                img={webformatURL}
+                alt={tags}
+                onClick={this.handleImageClick}
+                bigImg={largeImageURL}
+              />
+            );
           })}
         </ul>
         {images.length !== 0 && (
